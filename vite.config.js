f258476005b099682bf847642475746cfd4db334
Iryna <iryna.chumachenko@laba.academy>
2025-07-23 15:34:13 +0300

    changed runs-on

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,18 @@ import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+
   return {
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
     server: {
+      host: '0.0.0.0',
+      port: 3000,
       allowedHosts: ['.dev.acdm.space'],
+      hmr: {
+        host: 'vite-irina.dev.acdm.space',
+      },
     },
   };
 });
